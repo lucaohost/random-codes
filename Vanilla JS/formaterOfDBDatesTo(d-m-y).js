8eafc4100cd42f3449f3f7f9f9aaf3e665fc4128
Lucas Reginatto <lucas.reginatto.de.lima@gmail.com>
2018-10-16 20:25:26 -0300

    Update formaterOfDBDatesTo(d-m-y).js

--- Vanilla JS/formaterOfDBDatesTo(d-m-y).js
+++ Vanilla JS/formaterOfDBDatesTo(d-m-y).js
@@ -1,9 +1,21 @@
 <script>
-function formatDBToDMYDates(){
-    var obj = document.getElementsByClassName("myImputsWithDbDatesClass");
-    for (var prop in obj) {
-      var data = new Date(obj[prop].innerHTML + " 00:00:00");
-      obj[prop].innerHTML = data.toLocaleDateString('en-GB');  
+/*
+ *myVar can be two things:
+ *1.When you pass true in the second parameter, myVar i'ts considered a class 
+ *of elements that have dates which need to be formated.
+ *2.When you dont pass the second parameter, myVar i'ts considered the date 
+ *which need to be formated
+ */
+
+function formatDBToDMYDates(myVar, itsAClass = false){
+    if(itsAClass){
+        var obj = document.getElementsByClassName(myVar);
+        for (var prop in obj) {
+          obj[prop].innerHTML = new Date(obj[prop].innerHTML + " 00:00:00").toLocaleDateString('en-GB');
+        }
+    }else{
+        return new Date(myVar + " 00:00:00").toLocaleDateString('en-GB');
     }
 }
+
 </script>
