c3d9811eb4289432d814488185006d533765ac78
Lucas Reginatto <lucas.reginatto.de.lima@gmail.com>
2018-10-16 23:24:30 -0300

    Update findWord.js

--- Vanilla JS/findWord.js
+++ Vanilla JS/findWord.js
@@ -1,5 +1,10 @@
 <script>
-function findWord(word, string, ignoreCase) {
+/*
+ *returns true if the word exists in certain string
+ *if the third parameter is passed as false,the case
+ *of the word isn't ignored.
+*/
+function findWord(word, string, ignoreCase=true) {
   if(ignoreCase){
     word = word.toLowerCase();
     string = string.toLowerCase();
