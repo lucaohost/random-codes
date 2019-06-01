4bcb51b220a877f4b9d5bfa3ea060ebc25529548
Lucas Reginatto <lucas.reginatto.de.lima@gmail.com>
2019-05-31 23:13:41 -0300

    Update subDate.js

--- VanillaJS/subDate.js
+++ VanillaJS/subDate.js
@@ -3,6 +3,6 @@ function subDate(dbDate1, dbDate2) {
     // month is from 0 to 11
     date1 = new Date(parseInt(dateArray[0]), parseInt(dateArray[1]) -1, parseInt(dateArray[2]));
     let dateArray2 = dbDate2.split("-");
-    date1 = new Date(parseInt(dateArray2[0]), parseInt(dateArray2[1]) -1, parseInt(dateArray2[2]));
-    return Math.floor((new Date(dbDate1) - new Date(dbDate2)) / 86400000);
+    date2 = new Date(parseInt(dateArray2[0]), parseInt(dateArray2[1]) -1, parseInt(dateArray2[2]));
+    return Math.floor((date1 - date2) / 86400000);
 }
