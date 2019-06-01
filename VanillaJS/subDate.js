07e64eed5339d4df061b6da5801ebc7a498a9477
lucaxrl <lucas.reginatto.de.lima@gmail.com>
2019-05-31 23:08:32 -0300

    Fix in the subDate function

--- VanillaJS/subDate.js
+++ VanillaJS/subDate.js
@@ -1,13 +1,8 @@
 function subDate(dbDate1, dbDate2) {
+    let dateArray = dbDate1.split("-");
+    // month is from 0 to 11
+    date1 = new Date(parseInt(dateArray[0]), parseInt(dateArray[1]) -1, parseInt(dateArray[2]));
+    let dateArray2 = dbDate2.split("-");
+    date1 = new Date(parseInt(dateArray2[0]), parseInt(dateArray2[1]) -1, parseInt(dateArray2[2]));
     return Math.floor((new Date(dbDate1) - new Date(dbDate2)) / 86400000);
-}
-
-function subDateExplained(dbDate1, dbDate2) {
-    let date1InMiliseconds = new Date(dbDate1);
-    let date2InMiliseconds = new Date(dbDate2);
-    let differenceInMiliseconds = date1InMiliseconds - date2InMiliseconds;
-    const milisecondsInOneDay = 86400000;
-    let differenceInDays = differenceInMiliseconds / milisecondsInOneDay;
-    let differenceInCompletedDays = Math.floor(differenceInDays);
-    return differenceInCompletedDays;
 }
