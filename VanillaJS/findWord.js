605b2b36728a8710d53f47e3fa5ff9072601782b
lucaxrl <lucas.reginatto.de.lima@gmail.com>
2019-08-05 23:46:12 -0300

    Modify the docs of findWord function

--- VanillaJS/findWord.js
+++ VanillaJS/findWord.js
@@ -1,12 +1,12 @@
 /*
- *returns true if the word exists in certain string
- *if the third parameter is passed as false,the case
- *of the word isn't ignored.
+ * Returns true if the word exists in certain string.
+ * If the third parameter is passed as true, the
+ * comparission will be non case sensitive.
 */
 function findWord(word, string, ignoreCase = true) {
-  if(ignoreCase){
+  if (ignoreCase) {
     word = word.toLowerCase();
     string = string.toLowerCase();
   }
-  return string.split(' ').some(function(w){return w === word})
+  return string.split(' ').some(function (w) { return w === word })
 }
