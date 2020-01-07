d15b65834963e438059245057381a507eff8abe3
Lucas Reginatto <lucas.reginatto.de.lima@gmail.com>
2020-01-07 13:45:59 -0300

    Update findWord.js

--- VanillaJS/findWord.js
+++ VanillaJS/findWord.js
@@ -1,12 +1,12 @@
 /*
  * Returns true if the word exists in certain string.
- * If the third parameter is passed as true, the
- * comparission will be non case sensitive.
+ * If the third parameter is passed as false, the
+ * comparission will be case sensitive.
 */
-function findWord(word, string, ignoreCase = true) {
+function stringContains(word, string, ignoreCase = true) {
   if (ignoreCase) {
     word = word.toLowerCase();
     string = string.toLowerCase();
   }
-  return string.split(' ').some(function (w) { return w === word })
+  return string.search(word) > -1;
 }
