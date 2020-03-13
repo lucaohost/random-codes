c25576384d7baf930a5cc20198e6af5a5a92dc43
Lucas Reginatto <lucas.reginatto.de.lima@gmail.com>
2020-03-12 21:27:40 -0300

    Update combination.php

--- PHP/combination.php
+++ PHP/combination.php
@@ -13,5 +13,3 @@ function getCombinations($prefix, $chars) {
 $combinations = getCombinations('', ['L','U', 'C', 'A', 'S']);
 print_r($combinations);
 
-// pattern: when the elements are incresed in one, the number of combinations will duplicate and added 2.
-// example: 4 letters haves 14 combinations, while 5 letters will have 30 combinations ((14*2)+2) ...
