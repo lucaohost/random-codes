eba308edf34ce8b1d5199a81d6557fba6e3a2ce2
lucaohost <lucas.reginatto.de.lima@gmail.com>
2024-03-25 22:02:20 -0300

    add my aliases

---
+++ myBashrc.sh
@@ -0,0 +1,10 @@
+# Paste the code below in your ~/.bashrc
+# My Aliases
+# Go directory with all Github Projects.
+alias codes='cd /mnt/c/git/'
+
+# Remember my Bashes.
+alias rmb='ll ~/bashes'
+
+# Remeber my Aliases.
+alias rma="grep -A 1000000 -E ' My\s+Aliases' ~/.bashrc | tail -n +2"
