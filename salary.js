12dfdb8227c1681098ae9fe2b9f35e0e8c43473a
lucaohost <lucas.reginatto.de.lima@gmail.com>
2024-03-27 00:49:12 -0300

    Add another benefits to the calculation

--- salary.js
+++ salary.js
@@ -5,5 +5,10 @@ let salary13 = salary;
 let profitSharing = salary;
 let fgts = 100;
 let extraVacations = (1/3) * salary;
-let totalCompensation = (12 * salary) + (12 * fgts) + (foodVoucher * 12) + salary13 + profitSharing + (homeOfficeAssist * 12) + extraVacations;
+let healthInsurance = 0;
+let gymPass = 0;
+let dentalPlan = 0;
+let totalCompensation = (12 * salary) + (12 * fgts) + (foodVoucher * 12) +
+    salary13 + profitSharing + (homeOfficeAssist * 12) + extraVacations + 
+    (healthInsurance * 12) + (gymPass * 12) + (dentalPlan * 12);
 console.log("Total Compensation: ", Math.round(totalCompensation, 2), " Average per month: ", Math.round(totalCompensation/12, 2));
