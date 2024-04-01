c9c503190ea6a84bb3a11dabb50392f0d98f99fe
lucaohost <lucas.reginatto.de.lima@gmail.com>
2024-04-01 17:37:29 -0300

    Update filename

--- update-aliases.sh
+++ update-aliases.sh
@@ -5,12 +5,12 @@ cd /mnt/c/git/useful-examples
 
 echo "# Paste the code below in your ~/.bashrc" > aliases.sh
 
-rma >> aliases.sh
+grep -A 1000000 -E ' My\s+Aliases' ~/.bashrc | tail -n +2 >> aliases.sh
 
 git add aliases.sh > /dev/null 2>&1
 
 git commit -m "Update aliases" > /dev/null 2>&1
 
-git push origin main  > /dev/null 2>&1
+git push origin master  > /dev/null 2>&1
 
 cd "$folder_before_script" > /dev/null 2>&1
