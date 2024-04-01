86dc254c98b0309304718e2ef6124a5f13cdfae7
lucaohost <lucas.reginatto.de.lima@gmail.com>
2024-04-01 17:29:25 -0300

    Update aliases

--- aliases.sh
+++ aliases.sh
@@ -1,15 +1,1 @@
 # Paste the code below in your ~/.bashrc
-# Go directory with all Github Projects.
-alias codes='cd /mnt/c/git/'
-
-# Remember my Bashes.
-alias rmb='ll ~/bashes'
-
-# Remeber my Aliases.
-alias rma="grep -A 1000000 -E ' My\s+Aliases' ~/.bashrc | tail -n +2"
-
-# Close Terminal
-alias x="exit"
-
-# Turn off computer (doesn't work on WSL)
-alias xx="poweroff"
