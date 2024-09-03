ec0f3bcdfb458cf43c82ded3956ef78a381086ee
Lucas Reginatto de Lima <lucas.reginatto.de.lima@gmail.com>
2024-09-02 23:50:38 -0300

    Modify audio breaker script and description

--- audio-breaker/README.md
+++ audio-breaker/README.md
@@ -1,3 +1,3 @@
 This shell script processes all .ogg audio files in the current directory, splitting each file into two parts using ffmpeg.
 
-I made this, because bigger audios couldn't be player on 2x in Telegram.
+I made this, because when I send a audios bigger than 2 min, Telegram android app couldn't be play that audio on 2x.
--- audio-breaker/audio-breaker.sh
+++ audio-breaker/audio-breaker.sh
@@ -1,4 +1,4 @@
 for file in *.ogg; do
-  ffmpeg -i "$file" -ss 00:00:00 -to 00:01:50 -c copy "${file%.ogg}_part1.ogg" &&
-  ffmpeg -i "$file" -ss 00:01:50 -c copy "${file%.ogg}_part2.ogg"
+  ffmpeg -i "$file" -ss 00:00:00 -to 00:02:00 -c copy "${file%.ogg}_part1.ogg" &&
+  ffmpeg -i "$file" -ss 00:02:00 -c copy "${file%.ogg}_part2.ogg"
 done
