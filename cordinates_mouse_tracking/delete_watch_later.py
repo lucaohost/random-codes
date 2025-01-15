afcf1b3ac4766066c554e22cf3e98cdc98e0f91a
Lucas Reginatto de Lima <lucas.reginatto.de.lima@gmail.com>
2025-01-15 19:43:40 -0300

    feat: Make a loop for delete videos

--- cordinates_mouse_tracking/delete_watch_later.py
+++ cordinates_mouse_tracking/delete_watch_later.py
@@ -7,21 +7,25 @@ y_delete_button = 970
 import pyautogui
 import time
 
-# Give some time to prepare
-print("Starting in 5 seconds...")
-time.sleep(5)
+while True:
+    # Give some time to prepare
+    print("Starting in 5 seconds...")
+    time.sleep(5)
 
-# Click on option button
-x1, y1 = x_options_button, y_options_button
-pyautogui.click(x=x1, y=y1)
-print(f"Clicked at ({x1}, {y1})")
+    # Click on option button
+    x1, y1 = x_options_button, y_options_button
+    pyautogui.click(x=x1, y=y1)
+    print(f"Clicked at ({x1}, {y1})")
 
-time.sleep(2)
+    time.sleep(2)
 
-# Click on remove button
-x2, y2 = x_delete_button, y_delete_button  # Replace with your desired coordinates
-pyautogui.click(x=x2, y=y2)
-print(f"Clicked at ({x2}, {y2})")
+    # Click on remove button
+    x2, y2 = x_delete_button, y_delete_button  # Replace with your desired coordinates
+    pyautogui.click(x=x2, y=y2)
+    print(f"Clicked at ({x2}, {y2})")
+
+    time.sleep(2)
+    print("To stop script, press CTRL + C")
 
 
 
