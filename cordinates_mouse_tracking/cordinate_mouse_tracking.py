0ef8997bed407994190e8e2f83de73f8a8c23bc1
Lucas Reginatto de Lima <lucas.reginatto.de.lima@gmail.com>
2025-01-15 21:33:40 -0300

    feat: Add error handler on delete videos script

--- cordinates_mouse_tracking/cordinate_mouse_tracking.py
+++ cordinates_mouse_tracking/cordinate_mouse_tracking.py
@@ -1,12 +1,11 @@
 import pyautogui
 import time
 
-print("Press Ctrl+C to stop.")
-
 try:
     while True:
         x, y = pyautogui.position()
         print(f"Mouse position: (x:{x}, y:{y})")
-        time.sleep(1)  # Adjust the delay as needed
+        time.sleep(1)
+        print("Press Ctrl+C to stop.")
 except KeyboardInterrupt:
     print("\nTracking stopped.")
---
+++ cordinates_mouse_tracking/delete_youtube_watch_later_playlist.py
@@ -0,0 +1,37 @@
+import pyautogui
+import time
+import sys
+
+# In my screen x= 5544 y= 821
+# use coordinate_mouse_tracking to discover in yours
+x_options_button = 0
+y_options_button = 0
+
+# In my screen x= 5065 y= 1067
+# use coordinate_mouse_tracking to discover in yours
+x_delete_button = 0
+y_delete_button = 0
+
+if x_options_button == 0 or y_options_button == 0 or x_delete_button == 0 or y_delete_button == 0:
+    print("Error: x and y coordinates not defined, use cordinate_mouse_tracking.py to discover your coordinates.")
+    sys.exit()
+
+print("Starting in 5 seconds...")
+time.sleep(5)
+
+while True:
+    # Click on option button
+    x1, y1 = x_options_button, y_options_button
+    pyautogui.click(x=x1, y=y1)
+    print(f"Clicked at ({x1}, {y1})")
+
+    time.sleep(1)
+
+    # Click on remove button
+    x2, y2 = x_delete_button, y_delete_button  # Replace with your desired coordinates
+    pyautogui.click(x=x2, y=y2)
+    print(f"Clicked at ({x2}, {y2})")
+
+    time.sleep(1)
+    print("To stop script, press CTRL + C")
+    
